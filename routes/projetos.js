1eeb5089f9ed4717d10ceb099a7c57e73fd10478
4FunCode <vitvcz@gmail.com>
2024-11-06 16:22:53 -0300

    editar e excluir projetos

--- routes/projetos.js
+++ routes/projetos.js
@@ -8,10 +8,46 @@ const upload = require('../config/multer')
 router.get('/', async (req, res) => {
     try{
         const data = await Proj.aggregate([{ $sort: {date: -1 }}])
-        res.render('projetos', { data });
+
+        const proj_edit = req.query.proj_edit ? await Proj.findById(req.query.proj_edit) : undefined;
+
+        res.render('projetos', { data, proj_edit });
     }catch(error){
         console.log(`erro: ${error}`)
     }
+})
+
+router.get('/edit/:id', async(req, res) => {
+    let slug = req.params.id
+    res.redirect(`/projetos/?proj_edit=${slug}`)
+})
+
+router.post('/delete/:id', async (req, res)=>{
+    
+    const projectId = req.params.id;
+
+    await Proj.findByIdAndDelete(projectId)
+    
+    res.redirect('/projetos')   
+})
+
+router.put('/update/:id', upload, async (req, res)=>{
+
+    const projectId = req.params.id;
+    const titulo = req.body.title
+    const descr = req.body.desc
+    if(req.file){
+        const src = `imgs/uploads/${req.file.filename}`
+    }else{
+        src = Proj.findById(projectId).imgsrc
+    }
+
+    await Proj.findByIdAndUpdate(projectId, {title: titulo, desc: descr, imgsrc: src}, {
+        new: true,
+        runValidators: true
+    }) 
+
+    res.redirect('/projetos')
 })
 
 router.post('/upload', upload, async (req, res)=>{
@@ -20,7 +56,7 @@ router.post('/upload', upload, async (req, res)=>{
     const descr = req.body.desc
     const src = `imgs/uploads/${req.file.filename}`
 
-    const projeto = new Proj({title: titulo, desc: descr, imgsrc: src})
+    const projeto = new Proj({title: titulo, desc: descr, imgsrc: src}) 
 
     await projeto.save()
     res.redirect((req.get('referer')))
