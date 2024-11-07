9ea737c872eb4cfb963c17f4fea9dff3bc6ef111
4FunCode <vitvcz@gmail.com>
2024-11-07 09:52:22 -0300

    carrossel home funcional

--- routes/main.js
+++ routes/main.js
@@ -1,36 +1,39 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('Home');
+const Proj = require('../schemas/Projeto')
+
+router.get('/', async (req, res) => {
+    const data = await Proj.aggregate([{ $sort: {date: -1 }}, { $limit: 3 }])
+    res.render('Home', { data })
 })
 
 router.get('/Home', (req, res) => {
-    res.redirect('/');
+    res.redirect('/')
 })
 
 router.get('/NossaHistoria', (req, res) => {
-    res.render('NossaHistoria');
+    res.render('NossaHistoria')
 })
 
 router.get('/pick', (req, res) => {
-    res.render('pick');
+    res.render('pick')
 })
 
 router.get('/loginFunc', (req, res) => {
-    res.render('loginFunc');
+    res.render('loginFunc')
 })
 
 router.get('/loginMem', (req, res) => {
-    res.render('loginMem');
+    res.render('loginMem')
 })
 
 router.get('/ImpacTransp', (req, res) => {
-    res.render('ImpacTransp');
+    res.render('ImpacTransp')
 })
 
 router.get('/FAQ', (req, res) => {
-    res.render('FAQ');
+    res.render('FAQ')
 })
 
 
