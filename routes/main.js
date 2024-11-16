99ee7c3111ab9ddee2c4d233fe87c91ecaf272b1
Vítor <160747921+VPortoV@users.noreply.github.com>
2024-11-15 22:10:36 -0300

    política de privacidade

--- routes/main.js
+++ routes/main.js
@@ -46,6 +46,10 @@ router.get('/doacao_pix', (req, res) => {
     res.render('doacao_pix')
 })
 
+router.get('/PoliticaDePrivacidade', (req, res) => {
+    res.render('PoliticaDePrivacidade')
+})
+
 router.get('/auth-info', checkTokens, (req, res) => {
     if (req.user.isFunc){
         return res.status(200).json({ok: true, isFunc: true, mensagem: 'Usuário logado e tem permissão.'})
