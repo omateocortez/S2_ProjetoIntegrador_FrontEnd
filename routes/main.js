f15ca15950584a2b499250fca86730568fe8f455
omateocortez <mateovcortez@gmail.com>
2024-11-24 05:11:18 -0300

    update
    corrigi o footer, os problemas etc, e criei a dashboard, só que alguma coisa na rota não tá funfando então tem que digitar na URL direto msm... é só uma primeira. versão vejam o que seria legal de ter nessa dashboard e me diga abs

--- routes/main.js
+++ routes/main.js
@@ -54,6 +54,10 @@ router.get('/Certificacoes', (req, res) => {
     res.render('Certificacoes')
 })
 
+router.get('/DashAdmin', (req, res) => {
+    res.render('DashAdmin')
+})
+
 router.get('/auth-info', checkTokens, (req, res) => {
     if (req.user.isFunc){
         return res.status(200).json({ok: true, isFunc: true, mensagem: 'Usuário logado e tem permissão.'})
